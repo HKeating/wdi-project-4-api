53d48449d7176001ab8ed48a61bcd7b89f4f4aaf
HKeating <horacekeating@hotmail.co.uk>
2017-06-01 14:09:04 +0100

    register working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope :api do
+    resources :users
+    post 'register', to: 'authentications#register'
+    post 'login', to: 'authentications#login'
+  end
 end
