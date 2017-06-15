77ba4b837228ec97c9c5aa4c3d7e3a3ff484b23b
HKeating <horacekeating@hotmail.co.uk>
2017-06-15 22:52:29 +0100

    fleshed out seeds file for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ Milestone.destroy_all
 
 u1 = User.create!({
   username: "horace",
-  image: "http://fillmurray.com/200/200",
+  image: "https://scontent.xx.fbcdn.net/v/t1.0-1/p320x320/10432989_10153638017387575_814915425786401046_n.jpg?oh=dbfadd3a80acc32402ae8811699a4e59&oe=59D5B1FE",
   email: "horace@horace.com",
   password: "password",
   password_confirmation: "password"
@@ -22,7 +22,7 @@ u1 = User.create!({
 
 u2 = User.create!({
   username: "max",
-  image: "http://fillmurray.com/200/200",
+  image: "https://scontent.xx.fbcdn.net/v/t1.0-9/1619120_1026528287403836_8573424129763549103_n.jpg?oh=de923f2dd862803f5f13fa5c48fc8924&oe=59A54E52",
   email: "max@max.com",
   password: "password",
   password_confirmation: "password"
@@ -30,36 +30,67 @@ u2 = User.create!({
 
 u3 = User.create!({
   username: "harry",
-  image: "http://fillmurray.com/200/200",
+  image: "images/harry.png",
   email: "harry@harry.com",
   password: "password",
   password_confirmation: "password"
   })
 
+u4 = User.create!({
+    username: "alex",
+    image: "https://avatars0.githubusercontent.com/u/40461?v=3&s=400",
+    email: "alex@alex.com",
+    password: "password",
+    password_confirmation: "password"
+    })
+u5 = User.create!({
+    username: "lourenco",
+    image: "http://helppestcontrol.com/wp-content/uploads/2014/05/Pest-Control.gif",
+    email: "lourenco@lourenco.com",
+    password: "password",
+    password_confirmation: "password"
+    })
+u4 = User.create!({
+    username: "rane",
+    image: "http://cdn.images.express.co.uk/img/dynamic/67/590x/Giroud-641236.jpg",
+    email: "rane@rane.com",
+    password: "password",
+    password_confirmation: "password"
+    })
+
 p1 = Project.create!({
-  title: "wdi-project-4",
+  title: "Flappy Sausage",
   start_date: Date.parse('25-05-2017'),
   end_date: Date.parse('20-06-2017'),
   duration: 26,
-  image: "http://fillmurray.com/350/350",
+  image: "images/flappy_sausage.png",
   user_id: u1.id
   })
 
 p2 = Project.create!({
-  title: "test",
+  title: "Carrot Cake",
   start_date: Time.now,
   end_date: Date.parse('14-06-2017'),
   duration: 6,
-  image: "http://fillmurray.com/350/350",
+  image: "http://www.cookingclassy.com/wp-content/uploads/2012/01/carrot-cake-6-edit.jpg",
   user_id: u2.id
   })
 
 p3 = Project.create!({
-  title: "Testing the date stuff",
+  title: "Smart Fridge",
   start_date: Date.parse('01-06-2017'),
   end_date: Date.parse('16-06-2017'),
   duration: 15,
-  image: "http://fillmurray.com/350/350",
+  image: "http://media2.wptv.com//photo/2012/08/15/home_20120815112228_640_480.JPG",
+  user_id: u1.id
+  })
+
+p4 = Project.create!({
+  title: "Pad",
+  start_date: Date.parse('05-06-2017'),
+  end_date: Date.parse('26-06-2017'),
+  duration: 21,
+  image: "https://i.ytimg.com/vi/kSF534Blv6Q/maxresdefault.jpg",
   user_id: u1.id
   })
 
@@ -67,71 +98,94 @@ p3 = Project.create!({
 t1 = Task.create!({
   description: "back-end",
   start_day: 1,
-  due_day: 3,
+  due_day: 12,
   completed: false,
   blocked: false,
-  color: "EF6F6C",
+  color: "e74c3c",
   project_id: p1.id
 })
 t2 = Task.create!({
-  description: "task day 5",
+  description: "make sausage flappy",
   start_day: 2,
-  due_day: 5,
+  due_day: 10,
   completed: false,
   blocked: false,
-  color: "0074D9",
+  color: "2ecc71",
   project_id: p1.id
 })
 t3 = Task.create!({
-  description: "task day 6",
+  description: "font-end auth",
   start_day: 4,
-  due_day: 6,
+  due_day: 8,
   completed: false,
   blocked: false,
-  color: "b10dc9",
+  color: "2ecc71",
   project_id: p1.id
 })
 t4 = Task.create!({
-  description: "task day 3",
-  start_day: 1,
-  due_day: 3,
+  description: "team meeting",
+  start_day: 8,
+  due_day: 9,
   completed: false,
   blocked: false,
-  color: "AAAAAA",
+  color: "1abc9c",
   project_id: p1.id
   })
 t5 = Task.create!({
-  description: "task day 23",
-  start_day: 3,
+  description: "clean up the styling",
+  start_day: 22,
   due_day: 23,
   completed: false,
   blocked: false,
-  color: "AAAAAA",
+  color: "f1c40f",
   project_id: p1.id
   })
 t6 = Task.create!({
-  description: "task day 22",
-  start_day: 3,
-  due_day: 22,
+  description: "pictures",
+  start_day: 16,
+  due_day: 20,
   completed: false,
   blocked: false,
-  color: "AAAAAA",
+  color: "f1c40f",
+  project_id: p1.id
+  })
+t7 = Task.create!({
+  description: "build skynet",
+  start_day: 9,
+  due_day: 13,
+  completed: false,
+  blocked: false,
+  color: "e74c3c",
+  project_id: p1.id
+  })
+t8 = Task.create!({
+  description: "extra flappy sausage",
+  start_day: 18,
+  due_day: 23,
+  completed: false,
+  blocked: false,
+  color: "e74c3c",
   project_id: p1.id
   })
 
 m1 = p1.milestones.create!({
-  title: "mstone day 7",
+  title: "sausage",
   day: 7,
   completed: false
   })
 m2 = p1.milestones.create!({
-  title: "mstone day 3",
+  title: "machine learning",
   day: 3,
   completed: false
   })
 m3 = p1.milestones.create!({
-  title: "mstone day 10",
+  title: "html mockups",
   day: 10,
+  completed: false
+  })
+m4 = p1.milestones.create!({
+  title: "ship it",
+  day: 24,
   completed: false
   })
 
